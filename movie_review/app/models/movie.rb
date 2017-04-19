4ea523146a9df4e8f3ce53fbce0a34e396217ea5
Mario Rosenthal <mario.f.rosenthal@gmail.com>
2017-04-19 14:09:24 -0400

    added paperclip and image upload

--- movie_review/app/models/movie.rb
+++ movie_review/app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ApplicationRecord
   belongs_to :user
-  # has_attached_file :poster, styles: { medium: "400x600#" }
-  # validates_attachment_content_type :poster, content_type: /\Aimage\/.*\Z/
+  has_attached_file :image, styles: { medium: "400x600#" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
